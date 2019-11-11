a9c5d548c11bf4ea953a4e8225b7b4fc1dceb0a9
Lewis Cook <vulcan@wired.sh>
2019-11-11 19:39:27 +0000

    init.el: small tidy up, set new default font and theme.

--- init.el
+++ init.el
@@ -3,26 +3,14 @@
 (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
 
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (zenmint)))
+ '(custom-enabled-themes (quote (minty)))
  '(custom-safe-themes
    (quote
-    ("ce24bca1921ee2a35c9a1dd8912287a900d6eef0bd6d36c904eef87cf5eef1a6" default)))
- '(package-selected-packages
-   (quote
-    (htmlize ido-vertical-mode projectile magit hungry-delete agressive-indent flycheck yasnippet-snippets use-package try smex pretty-mode eyebrowse counsel company-irony beacon auto-complete)))
- '(sp-escape-quotes-after-insert nil))
+    ("49575f390f6c920fa690ffd066b59f4fbf644dd2265c24789db9d942e2c0a32f" default)))
+ '((setq)p-escape-quotes-after-insert nil))
+
 (set-face-attribute 'default nil
-                    :family "Iosevka"
+                    :family "VictorMono"
                     :height 130
                     :weight 'normal
                     :width 'normal)
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
